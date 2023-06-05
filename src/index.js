69207fc29936283b582426ac564929a4102d4ece
iam454 <realseogy@gmail.com>
2023-06-05 11:24:33 +0900

    feat: 페이지 로드시 id에 autofocus

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 // TODO: 이 곳에 정답 코드를 작성해주세요.
-console.log("hello");
+
+// 요구사항 1
+/* -------------------------------------------- */
+// 방법 1
+const id = document.getElementById("id");
+// const $id = document.getElementById("id");
+// $변수 : DOM을 통해 가져온 변수
+
+window.addEventListener("load", () => {
+  id.focus();
+});
+
+// 방법 2
+// input attr에 autofocus를 직접 부여
